f24eae72d9f97a9e579cf883d9e8968d8235d914
patrick brisbin <pbrisbin@gmail.com>
2018-04-22 08:59:16 -0400

    Re-structure Spec helpers
    Try to keep only re-exports and globally-useful stuff in SpecHelper.
    Makes it easier to clean up dead code when we simplify other things.

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -3,24 +3,14 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module SpecHelper
-    ( module SpecHelper
-    , module X
-    )
-    where
+    ( module X
+    ) where
 
-import ClassyPrelude as X hiding (dropEnd)
-
-import Data.Char (isSpace)
-import Data.Proxy
-import qualified Data.Text as T
-import GitHub.Data (Id, mkId)
-import qualified Prelude as P
-import Restyler.Process as X (callProcess)
-import System.Directory (removeFile, setCurrentDirectory)
-import System.IO.Temp as X (emptySystemTempFile, withSystemTempDirectory)
-import System.Process as X (readProcess)
+import ClassyPrelude as X
 import Test.Hspec as X
-import Text.Shakespeare.Text as X (st)
+
+import Data.Proxy
+import GitHub.Data (Id, mkId)
 
 instance Num (Id a) where
     -- Just so we can type literals for Ids in Specs
@@ -31,41 +21,3 @@ instance Num (Id a) where
     (*) = error "NO"
     abs = error "NO"
     signum = error "NO"
-
-setupGitRepo :: FilePath -> IO ()
-setupGitRepo dir = do
-    setCurrentDirectory dir
-    callProcess "git" ["init"]
-    callProcess "git" ["commit", "--allow-empty", "--message", "Test"]
-
--- | Dedent content
---
--- N.B. assumes an initial "\n" and trailing quote-end, which is what happens
--- when you use the st quasi-quoter in the following style:
---
--- > [st|
--- >   some code
--- >   some code
--- >   |]
---
--- In other words, do not use this on (e.g.) @\"Some text\n\"@
---
-dedent :: Text -> Text
-dedent t = T.unlines $ map (T.drop $ indent lns) lns
-  where
-    lns = dropEnd $ T.lines $ T.drop 1 t
-    dropEnd = reverse . drop 1 . reverse
-
-    indent [] = 0
-    indent ts = P.minimum $ map (T.length . T.takeWhile isSpace) ts
-
-withEmptySystemTempFile :: (FilePath -> IO a) -> IO a
-withEmptySystemTempFile = bracket (emptySystemTempFile "") removeFile
-
-hasError :: String -> Either String b -> Bool
-hasError msg (Left err) = msg `isInfixOf` err
-hasError _ _ = False
-
-isLeft :: Either a b -> Bool
-isLeft (Left _) = True
-isLeft _ = False
