31d1d6382f32c72d294d8e8f7953fc55f3fd2f6f
patrick brisbin <pbrisbin@gmail.com>
2018-04-23 17:35:31 -0400

    Move App module to top-level
    We should be able to re-use this to provide logging and global
    configuration the main executable as well.

---
+++ src/Restyler/App.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE RecordWildCards #-}
+module Restyler.App
+    ( App(..)
+    , AppM
+    , runApp
+    , loadApp
+    , module Control.Monad.Logger
+    , module Control.Monad.Reader
+    ) where
+
+import Control.Monad.Logger
+import Control.Monad.Reader
+import qualified Env
+
+data App c = App
+    { appConfig :: c
+    , appLogLevel :: LogLevel
+    }
+
+type AppM c = ReaderT (App c) (LoggingT IO)
+
+loadApp :: c -> IO (App c)
+loadApp c = Env.parse id $ App
+    <$> pure c
+    <*> Env.flag LevelInfo LevelDebug "DEBUG" mempty
+
+runApp :: App c -> AppM c a -> IO a
+runApp app@App{..} action = runStdoutLoggingT
+    $ filterLogger (\_ level -> level >= appLogLevel)
+    $ runReaderT action app
