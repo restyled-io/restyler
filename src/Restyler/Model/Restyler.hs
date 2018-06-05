e0b7a4c847b3680490b50f772e0d1ab597c97e0e
patrick brisbin <pbrisbin@gmail.com>
2018-06-05 06:52:40 -0400

    Pass --fail-level fatal to rubocop by default

--- src/Restyler/Model/Restyler.hs
+++ src/Restyler/Model/Restyler.hs
@@ -152,7 +152,7 @@ allRestylers =
     , Restyler
         { rName = "rubocop"
         , rCommand = "rubocop"
-        , rArguments = ["--auto-correct"]
+        , rArguments = ["--auto-correct", "--fail-level fatal"]
         , rInclude = ["**/*.rb"]
         , rInterpreters = [Ruby]
         , rSupportsArgSep = True
