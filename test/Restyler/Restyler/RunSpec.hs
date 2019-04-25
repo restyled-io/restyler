93bf563e630c2a9065c00b2e903d59210a0dae3d
patrick brisbin <pbrisbin@gmail.com>
2019-04-25 17:36:33 -0400

    Generalize TestApp spike
    Use a single TestApp via SpecHelper and change behavior by construction.
    Then we can change specific behaviors of interest in the given spec.
    
    Move someRestyler by the same principles.

--- test/Restyler/Restyler/RunSpec.hs
+++ test/Restyler/Restyler/RunSpec.hs
@@ -5,43 +5,24 @@ where
 
 import SpecHelper
 
-import Restyler.App.Class
 import Restyler.App.Error
 import Restyler.Config.Interpreter
 import Restyler.Restyler
 import Restyler.Restyler.Run
 
-data TestApp = TestApp
-
-instance HasSystem TestApp where
-    -- Must throw AppError to match real implementation
-    readFile = handleIO (throwIO . SystemError) . readFileUtf8
-
-    -- Not used in this test
-    getCurrentDirectory = error "getCurrentDirectory"
-    setCurrentDirectory = error "setCurrentDirectory"
-    doesFileExist = error "doesFileExist"
-
-runTestApp :: RIO TestApp a -> IO a
-runTestApp = runRIO TestApp
-
 spec :: Spec
 spec = do
     describe "filterRestylePaths" $ do
         it "ignores unreadable (invalid utf-8 byte) files" $ do
-            let invalidFile = "test/files/AsanaMathJax_Alphabets-Regular.eot"
-                someRestyler = Restyler
-                    { rName = "test-restyler"
-                    , rImage = "restyled/restyler-test-restyler"
-                    , rCommand = ["restyle"]
-                    , rArguments = []
-                    , rInclude = ["!**/*.eot"]
+            let errReadFile = mapAppError SystemError . readFileUtf8
+                runTestApp = runRIO testApp { taReadFile = errReadFile }
+                restyler = someRestyler
+                    { rInclude = ["!**/*.eot"]
                     , rInterpreters = [Ruby]
-                    , rSupportsArgSep = True
-                    , rSupportsMultiplePaths = True
                     }
+                invalidFile = "test/files/AsanaMathJax_Alphabets-Regular.eot"
 
             filtered <- runTestApp $ do
-                filterRestylePaths someRestyler [invalidFile]
+                filterRestylePaths restyler [invalidFile]
 
             filtered `shouldBe` []
