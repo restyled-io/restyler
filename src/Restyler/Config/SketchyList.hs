3916cb1123ed9937e3eec2b6e6ad0c44f7c50c27
patrick brisbin <pbrisbin@gmail.com>
2019-06-28 08:42:33 -0400

    Remove unused code

--- src/Restyler/Config/SketchyList.hs
+++ src/Restyler/Config/SketchyList.hs
@@ -1,7 +1,5 @@
 module Restyler.Config.SketchyList
     ( SketchyList
-    , sketchyOne
-    , sketchyMany
     , unSketchy
     )
 where
@@ -13,18 +11,6 @@ import Data.Aeson.Types (typeMismatch)
 
 data SketchyList a = One a | Many [a]
     deriving (Eq, Show)
-
-instance Semigroup (SketchyList a) where
-    a <> b = Many $ unSketchy a <> unSketchy b
-
-instance Monoid (SketchyList a) where
-    mempty = Many []
-
-sketchyOne :: a -> SketchyList a
-sketchyOne = One
-
-sketchyMany :: [a] -> SketchyList a
-sketchyMany = Many
 
 unSketchy :: SketchyList a -> [a]
 unSketchy (One i) = [i]
