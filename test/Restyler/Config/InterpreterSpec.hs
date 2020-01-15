5b20099b581982654cccf9ee676933284e8beec9
patrick brisbin <pbrisbin@gmail.com>
2020-01-15 10:54:26 -0500

    hspec-expectations-lifted

--- test/Restyler/Config/InterpreterSpec.hs
+++ test/Restyler/Config/InterpreterSpec.hs
@@ -11,18 +11,18 @@ import Restyler.Config.Interpreter
 spec :: Spec
 spec = do
     describe "hasIntepreter" $ do
-        it "can find a direct executable" $ do
+        it "can find a direct executable" $ example $ do
             let contents = T.unlines ["#!/bin/sh", "echo 1"]
 
             (contents `hasInterpreter` Sh) `shouldBe` True
             (contents `hasInterpreter` Bash) `shouldBe` False
 
-        it "can find a nested executable" $ do
+        it "can find a nested executable" $ example $ do
             let contents = T.unlines ["#!/usr/local/bin/bash", "echo 1"]
 
             (contents `hasInterpreter` Bash) `shouldBe` True
 
-        it "can find something via /usr/bin/env" $ do
+        it "can find something via /usr/bin/env" $ example $ do
             let contents = T.unlines ["#!/usr/bin/env python", "print 1"]
 
             (contents `hasInterpreter` Python) `shouldBe` True
