c6971fee94651d7d993689b0c6f026d6a806ef35
patrick brisbin <pbrisbin@gmail.com>
2021-06-30 10:31:57 -0400

    Increment finished/success metrics on Restyles

--- app/main.hs
+++ app/main.hs
@@ -10,6 +10,7 @@ import Restyler.App.Error
 import Restyler.Main
 import Restyler.Options
 import Restyler.Statsd (withStatsClient)
+import qualified Restyler.Statsd as Statsd
 
 main :: IO ()
 main = do
@@ -23,4 +24,10 @@ main = do
             withSystemTempDirectory "restyler-" $ \path -> do
                 app <- bootstrapApp options path statsClient
                 runRIO app $ handleAny errorPullRequest restylerMain
-        runRIO statsClient $ either dieAppError pure result
+
+        runRIO statsClient $ do
+            Statsd.increment "restyler.finished" []
+            either
+                dieAppError -- includes .error increment
+                (\() -> Statsd.increment "restyler.success" [])
+                result
