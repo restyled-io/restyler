9a209173867c88a2ce3b877ac43c9f54a3cfa608
patrick brisbin <pbrisbin@gmail.com>
2024-07-17 10:38:18 -0400

    Re-org 4

--- restyle-gha/main.hs
+++ restyle-gha/main.hs
@@ -4,77 +4,12 @@ module Main
 
 import Restyler.Prelude
 
-import Env qualified
-import Restyler.App (AppT)
 import Restyler.CLI qualified as CLI
-import Restyler.Commands.RestyleGHA
 import Restyler.GHA
-import Restyler.Git (ActualGit (..), MonadGit)
-import Restyler.GitHub.Api
-import Restyler.HostDirectoryOption
-import Restyler.ImageCleanupOption
-import Restyler.LogSettingsOption
-import Restyler.ManifestOption
-import Restyler.Opt (PullRequestOption)
-import Restyler.Opt qualified as Opt
-import Restyler.Options.RestyleGHA
-import Restyler.Options.RestyleLocal
-  ( HasOptions (..)
-  , Options (..)
-  , ThroughOptions (..)
-  )
-import Restyler.Options.RestyleLocal qualified as RestyleLocal
-import Restyler.Restrictions
+import Restyler.GHA.App
 
-data App = App
-  { logger :: Logger
-  , env :: EnvOptions
-  , options :: Options
-  , pullRequest :: PullRequestOption
-  }
-  deriving (HasHostDirectoryOption) via (ThroughOptions App)
-  deriving (HasImageCleanupOption) via (ThroughOptions App)
-  deriving (HasManifestOption) via (ThroughOptions App)
-  deriving (HasRestrictions) via (ThroughOptions App)
-
-instance HasOptions App where
-  getOptions = (.options)
-
-envL :: Lens' App EnvOptions
-envL = lens (.env) $ \x y -> x {env = y}
-
-instance HasLogger App where
-  loggerL = lens (.logger) $ \x y -> x {logger = y}
-
-instance HasGitHubToken App where
-  githubTokenL = envL . githubTokenL
-
-instance HasGitHubOutput App where
-  githubOutputL = envL . githubOutputL
-
-deriving via
-  (ActualGit (AppT App m))
-  instance
-    MonadUnliftIO m => MonadGit (AppT App m)
-
-withApp :: (App -> IO a) -> IO a
-withApp f = do
-  (env, envLocal) <-
-    Env.parse id
-      $ (,)
-      <$> envParser
-      <*> RestyleLocal.envParser
-
-  (optLocal, pr) <-
-    Opt.parse "Restyle on GitHub Actions"
-      $ (,)
-      <$> RestyleLocal.optParser
-      <*> Opt.optPullRequest
-
-  let options = envLocal <> optLocal
-
-  withLogger (resolveLogSettings options.logSettings) $ \logger -> do
-    f $ App {logger = logger, env = env, options = options, pullRequest = pr}
+-- TODO: Restyler.Options.PullRequest
+import Restyler.Opt (PullRequestOption (..))
 
 main :: IO ()
 main = CLI.main withApp $ do
---
+++ src/Restyler/GHA/App.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
+module Restyler.GHA.App
+  ( App (..)
+  , withApp
+  ) where
+
+import Restyler.Prelude
+
+import Env qualified
+import Restyler.App (AppT)
+import Restyler.GHA.Output
+import Restyler.Git (ActualGit (..), MonadGit)
+import Restyler.GitHub.Api
+import Restyler.GitHubEnv
+import Restyler.Local.Options
+import Restyler.Opt (PullRequestOption)
+import Restyler.Opt qualified as Opt
+import Restyler.Options.HostDirectory
+import Restyler.Options.ImageCleanup
+import Restyler.Options.LogSettings
+import Restyler.Options.Manifest
+import Restyler.Restrictions
+
+data App = App
+  { logger :: Logger
+  , options :: Options
+  , githubEnv :: GitHubEnv
+  , pullRequest :: PullRequestOption
+  }
+  deriving (HasHostDirectoryOption) via (ThroughOptions App)
+  deriving (HasImageCleanupOption) via (ThroughOptions App)
+  deriving (HasManifestOption) via (ThroughOptions App)
+  deriving (HasRestrictions) via (ThroughOptions App)
+
+instance HasOptions App where
+  getOptions = (.options)
+
+githubEnvL :: Lens' App GitHubEnv
+githubEnvL = lens (.githubEnv) $ \x y -> x {githubEnv = y}
+
+instance HasLogger App where
+  loggerL = lens (.logger) $ \x y -> x {logger = y}
+
+instance HasGitHubToken App where
+  githubTokenL = githubEnvL . githubTokenL
+
+instance HasGitHubOutput App where
+  githubOutputL = githubEnvL . githubOutputL
+
+deriving via
+  (ActualGit (AppT App m))
+  instance
+    MonadUnliftIO m => MonadGit (AppT App m)
+
+withApp :: (App -> IO a) -> IO a
+withApp f = do
+  (githubEnv, env) <-
+    Env.parse id
+      $ (,)
+      <$> githubEnvParser
+      <*> envParser
+
+  (opt, pullRequest) <-
+    Opt.parse "Restyle on GitHub Actions"
+      $ (,)
+      <$> optParser
+      <*> Opt.optPullRequest
+
+  let options = env <> opt
+
+  withLogger (resolveLogSettings options.logSettings) $ \logger -> do
+    f $ App {logger, options, githubEnv, pullRequest}
