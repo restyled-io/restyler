1c501847fb7e657ea1140fedb836d49bb27d738f
patrick brisbin <pbrisbin@gmail.com>
2024-07-17 10:48:25 -0400

    Finish re-org

--- restyle-path/main.hs
+++ restyle-path/main.hs
@@ -4,54 +4,9 @@ module Main
 
 import Restyler.Prelude
 
-import Data.List.NonEmpty (some1)
-import Env qualified
-import Restyler.App (AppT)
 import Restyler.CLI qualified as CLI
-import Restyler.Commands.RestyleLocal
-import Restyler.Git (ActualGit (..), MonadGit)
-import Restyler.HostDirectoryOption
-import Restyler.ImageCleanupOption
-import Restyler.LogSettingsOption
-import Restyler.ManifestOption
-import Restyler.Opt qualified as Opt
-import Restyler.Options.RestyleLocal
-import Restyler.Restrictions
-
-data App = App
-  { logger :: Logger
-  , options :: Options
-  , paths :: NonEmpty FilePath
-  }
-  deriving (HasHostDirectoryOption) via (ThroughOptions App)
-  deriving (HasImageCleanupOption) via (ThroughOptions App)
-  deriving (HasManifestOption) via (ThroughOptions App)
-  deriving (HasRestrictions) via (ThroughOptions App)
-
-instance HasOptions App where
-  getOptions = (.options)
-
-instance HasLogger App where
-  loggerL = lens (.logger) $ \x y -> x {logger = y}
-
-deriving via
-  (ActualGit (AppT App m))
-  instance
-    MonadUnliftIO m => MonadGit (AppT App m)
-
-withApp :: (App -> IO a) -> IO a
-withApp f = do
-  env <- Env.parse id envParser
-  (opt, paths) <-
-    Opt.parse "Restyle local files"
-      $ (,)
-      <$> optParser
-      <*> some1 (Opt.argument Opt.str $ Opt.metavar "PATH")
-
-  let options = env <> opt
-
-  withLogger (resolveLogSettings options.logSettings) $ \logger -> do
-    f $ App {logger = logger, options = options, paths = paths}
+import Restyler.Local
+import Restyler.Local.App
 
 main :: IO ()
 main = CLI.main withApp $ do
---
+++ src/Restyler/Local/App.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
+module Restyler.Local.App
+  ( App (..)
+  , withApp
+  ) where
+
+import Restyler.Prelude
+
+import Data.List.NonEmpty (some1)
+import Env qualified
+import Restyler.App (AppT)
+import Restyler.Git (ActualGit (..), MonadGit)
+import Restyler.Local.Options
+import Restyler.Opt qualified as Opt
+import Restyler.Options.HostDirectory
+import Restyler.Options.ImageCleanup
+import Restyler.Options.LogSettings
+import Restyler.Options.Manifest
+import Restyler.Restrictions
+
+data App = App
+  { logger :: Logger
+  , options :: Options
+  , paths :: NonEmpty FilePath
+  }
+  deriving (HasHostDirectoryOption) via (ThroughOptions App)
+  deriving (HasImageCleanupOption) via (ThroughOptions App)
+  deriving (HasManifestOption) via (ThroughOptions App)
+  deriving (HasRestrictions) via (ThroughOptions App)
+
+instance HasOptions App where
+  getOptions = (.options)
+
+instance HasLogger App where
+  loggerL = lens (.logger) $ \x y -> x {logger = y}
+
+deriving via
+  (ActualGit (AppT App m))
+  instance
+    MonadUnliftIO m => MonadGit (AppT App m)
+
+withApp :: (App -> IO a) -> IO a
+withApp f = do
+  env <- Env.parse id envParser
+  (opt, paths) <-
+    Opt.parse "Restyle local files"
+      $ (,)
+      <$> optParser
+      <*> some1 (Opt.argument Opt.str $ Opt.metavar "PATH")
+
+  let options = env <> opt
+
+  withLogger (resolveLogSettings options.logSettings) $ \logger -> do
+    f $ App {logger, options, paths}
