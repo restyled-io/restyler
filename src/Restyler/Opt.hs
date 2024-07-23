84ce8d54a77940b8836594ba7d0557c9ba7cd55a
patrick brisbin <pbrisbin@gmail.com>
2024-07-23 09:16:58 -0400

    Address ENV parsing errors on --help
    Since `restyle-gha` has required ENV (`GITHUB_TOKEN`), it was failing
    when we run `--help`. We can correct this by parsing ENV after options.
    
    Additionally, we can extract the help from the ENV parser and add it as
    a footer to `--help`. That help doc isn't great, but it's better than
    nothing for now, and we may upstream improvements.

--- src/Restyler/Opt.hs
+++ src/Restyler/Opt.hs
@@ -6,12 +6,36 @@ where
 
 import Restyler.Prelude
 
+import Data.List qualified as List
 import Options.Applicative
+import Options.Applicative.Help.Chunk
 
 parse
   :: String
   -- ^ Description
+  -> String
+  -- ^ Footer
   -> Parser a
   -- ^ Options parser
   -> IO a
-parse d p = execParser $ info (p <**> helper) $ fullDesc <> progDesc d
+parse d f p =
+  execParser
+    $ info (p <**> helper)
+    $ fullDesc
+    <> progDesc d
+    <> footerLines f
+
+-- |
+--
+-- The 'footer' function takes a string, but then makes it a doc with
+-- 'paragraph' which replaces all newlines with spaces. Fun. This function
+-- preserves newlines at least, so the simple case of a pre-pretty-printed
+-- string is hanlded somewhat better.
+footerLines :: String -> InfoMod a
+footerLines =
+  footerDoc
+    . Just
+    . extractChunk
+    . vcatChunks
+    . map stringChunk
+    . List.lines
