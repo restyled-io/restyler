22c6afde39593f49adc02f327f2a5d0bc0285a11
patrick brisbin <pbrisbin@gmail.com>
2024-10-19 11:44:22 -0400

    Test actions/run CLI options

--- test/Restyler/ConfigSpec.hs
+++ test/Restyler/ConfigSpec.hs
@@ -14,6 +14,7 @@ module Restyler.ConfigSpec
 
 import Restyler.Prelude
 
+import Blammo.Logging.LogSettings (defaultLogSettings, shouldLogLevel)
 import Data.Text.IO (hPutStr)
 import OptEnvConf.Args (parseArgs)
 import OptEnvConf.EnvMap qualified as EnvMap
@@ -30,188 +31,212 @@ import UnliftIO.Temporary (withSystemTempFile)
 
 spec :: Spec
 spec = do
-  -- This test is a maintainence burden, in that when config/default.yaml
-  -- changes, we need a corresponding update here. But it ensures we don't
-  -- unintentionally break loading the defaults.
-  it "uses defined defaults" $ do
-    config <- loadTestConfig [] [] ["Foo.hs"]
+  context "command-line" $ do
+    context "flags used by actions/run" $ do
+      let checkOption name p = do
+            it ("supports --" <> name) $ do
+              config <- loadTestConfig [] [] ["--" <> name, "Foo.hs"]
+              p config `shouldBe` True
 
-    -- config.logSettings `shouldBe` _
-    config.enabled `shouldBe` True
-    config.exclude
-      `shouldBe` [ "**/*.patch"
-                 , "**/.git/**/*"
-                 , "**/node_modules/**/*"
-                 , "**/vendor/**/*"
-                 , ".github/workflows/**/*"
-                 ]
-    config.dryRun `shouldBe` False
-    config.failOnDifferences `shouldBe` False
-    config.commitTemplate `shouldBe` "Restyled by ${restyler.name}\n"
-    config.remoteFiles `shouldBe` []
-    config.ignores
-      `shouldBe` Ignores
-        { byAuthor = ["*[bot]"]
-        , byBranch = ["renovate/*"]
-        , byLabels = ["restyled-ignore"]
-        }
-    config.restylersVersion `shouldBe` "stable"
-    config.restylersManifest `shouldBe` Nothing
-    config.restylerOverrides `shouldBe` [wildcard]
-    -- config.hostDirectory `shouldBe` _
-    config.imageCleanup `shouldBe` False
-    config.noCommit `shouldBe` False
-    config.noClean `shouldBe` False
-    config.noPull `shouldBe` False
-    config.restrictions
-      `shouldBe` Restrictions
-        { netNone = True
-        , cpuShares = Just 512
-        , memory = Just $ Bytes {number = 128, suffix = Just M}
-        }
-    config.pullRequestJson `shouldBe` Nothing
-    config.paths `shouldBe` pure "Foo.hs"
+      checkOption "debug" $ \config ->
+        shouldLogLevel (config.logSettings defaultLogSettings) "" LevelDebug
 
-  it "can be overriden in user-configuration" $ do
-    config <-
-      loadTestConfig
-        [ "enabled: false"
-        , "exclude: [a, b]"
-        , "dry_run: true"
-        , "fail_on_differences: true"
-        , "also_exclude: [c, d]"
-        , "commit_template: Hi"
-        , "remote_files:"
-        , "  - url: https://example.com"
-        , "    path: example.txt"
-        , "ignore:"
-        , "  authors: [x]"
-        , "  branches: ['y']"
-        , "  labels: []"
-        , "restylers_version: dev"
-        , "restylers: [fourmolu]"
-        , "docker:"
-        , "  image_cleanup: true"
-        , "  pull: false"
-        , "  restylers:"
-        , "    net_none: false"
-        , "    cpu_shares: 1024"
-        , "    memory: { number: 512, suffix: k }"
-        , "git:"
-        , "  clean: false"
-        , "  commit: false"
-        ]
-        []
-        ["Bar.hs"]
+      checkOption "fail-on-differences" (.failOnDifferences)
 
-    -- config.logSettings `shouldBe` _
-    config.enabled `shouldBe` False
-    config.exclude `shouldBe` ["a", "b", "c", "d"]
-    config.dryRun `shouldBe` True
-    config.failOnDifferences `shouldBe` True
-    config.commitTemplate `shouldBe` "Hi"
-    config.remoteFiles
-      `shouldBe` [RemoteFile {url = "https://example.com", path = "example.txt"}]
-    config.ignores
-      `shouldBe` Ignores
-        { byAuthor = ["x"]
-        , byBranch = ["y"]
-        , byLabels = []
-        }
-    config.restylersVersion `shouldBe` "dev"
-    config.restylersManifest `shouldBe` Nothing
-    config.restylerOverrides `shouldBe` [enabled "fourmolu"]
-    -- config.hostDirectory `shouldBe` _
-    config.imageCleanup `shouldBe` True
-    config.noCommit `shouldBe` True
-    config.noClean `shouldBe` True
-    config.noPull `shouldBe` True
-    config.restrictions
-      `shouldBe` Restrictions
-        { netNone = False
-        , cpuShares = Just 1024
-        , memory = Just $ Bytes {number = 512, suffix = Just K}
-        }
-    config.pullRequestJson `shouldBe` Nothing
-    config.paths `shouldBe` pure "Bar.hs"
+      checkOption "image-cleanup" (.imageCleanup)
 
-  context "restyler overrides" $ do
-    it "supports names" $ do
-      config <-
-        loadTestConfig
-          [ "restylers:"
-          , "  - fourmolu"
-          , "  - \"!stylish-haskell\""
-          ]
-          []
-          ["x.hs"]
+      checkOption "no-commit" (.noCommit)
 
-      config.restylerOverrides
-        `shouldBe` [enabled "fourmolu", disabled "stylish-haskell"]
+      checkOption "no-pull" (.noPull)
 
-    it "supports name-keys" $ do
-      config <-
-        loadTestConfig
-          [ "restylers:"
-          , "  - fourmolu:"
-          , "      enabled: true"
-          , "  - stylish-haskell:"
-          , "      enabled: false"
-          ]
-          []
-          ["x.hs"]
+  -- TODO: need to assert on Path Abs File
+  -- it "supports --manifest" $ do
+  --   config <- loadTestConfig [] [] ["--manifest", "/tmp/x.yaml", "Foo.hs"]
+  --   config.restylersManifest `shouldBe` Just "/tmp/x.yaml"
 
-      config.restylerOverrides
-        `shouldBe` [enabled "fourmolu", disabled "stylish-haskell"]
+  context "configuration" $ do
+    -- This test is a maintainence burden, in that when config/default.yaml
+    -- changes, we need a corresponding update here. But it ensures we don't
+    -- unintentionally break loading the defaults.
+    it "uses defined defaults" $ do
+      config <- loadTestConfig [] [] ["Foo.hs"]
 
-    it "supports objects with names" $ do
-      config <-
-        loadTestConfig
-          [ "restylers:"
-          , "  - name: fourmolu"
-          , "    enabled: true"
-          , "  - name: stylish-haskell"
-          , "    enabled: false"
-          ]
-          []
-          ["x.hs"]
-
-      config.restylerOverrides
-        `shouldBe` [enabled "fourmolu", disabled "stylish-haskell"]
-
-  context "legacy ignore options" $ do
-    it "fully specified" $ do
-      config <-
-        loadTestConfig
-          [ "ignore_authors: [a]"
-          , "ignore_branches: [b]"
-          , "ignore_labels: [c]"
-          ]
-          []
-          ["Bar.hs"]
-
+      -- config.logSettings `shouldBe` _
+      config.enabled `shouldBe` True
+      config.exclude
+        `shouldBe` [ "**/*.patch"
+                   , "**/.git/**/*"
+                   , "**/node_modules/**/*"
+                   , "**/vendor/**/*"
+                   , ".github/workflows/**/*"
+                   ]
+      config.dryRun `shouldBe` False
+      config.failOnDifferences `shouldBe` False
+      config.commitTemplate `shouldBe` "Restyled by ${restyler.name}\n"
+      config.remoteFiles `shouldBe` []
       config.ignores
         `shouldBe` Ignores
-          { byAuthor = ["a"]
-          , byBranch = ["b"]
-          , byLabels = ["c"]
-          }
-
-    it "partially specified" $ do
-      config <-
-        loadTestConfig
-          [ "ignore_authors: [a]"
-          , "ignore_labels: [c]"
-          ]
-          []
-          ["Bar.hs"]
-
-      config.ignores
-        `shouldBe` Ignores
-          { byAuthor = ["a"]
+          { byAuthor = ["*[bot]"]
           , byBranch = ["renovate/*"]
-          , byLabels = ["c"]
+          , byLabels = ["restyled-ignore"]
           }
+      config.restylersVersion `shouldBe` "stable"
+      config.restylersManifest `shouldBe` Nothing
+      config.restylerOverrides `shouldBe` [wildcard]
+      -- config.hostDirectory `shouldBe` _
+      config.imageCleanup `shouldBe` False
+      config.noCommit `shouldBe` False
+      config.noClean `shouldBe` False
+      config.noPull `shouldBe` False
+      config.restrictions
+        `shouldBe` Restrictions
+          { netNone = True
+          , cpuShares = Just 512
+          , memory = Just $ Bytes {number = 128, suffix = Just M}
+          }
+      config.pullRequestJson `shouldBe` Nothing
+      config.paths `shouldBe` pure "Foo.hs"
+
+    it "can be overriden in user-configuration" $ do
+      config <-
+        loadTestConfig
+          [ "enabled: false"
+          , "exclude: [a, b]"
+          , "dry_run: true"
+          , "fail_on_differences: true"
+          , "also_exclude: [c, d]"
+          , "commit_template: Hi"
+          , "remote_files:"
+          , "  - url: https://example.com"
+          , "    path: example.txt"
+          , "ignore:"
+          , "  authors: [x]"
+          , "  branches: ['y']"
+          , "  labels: []"
+          , "restylers_version: dev"
+          , "restylers: [fourmolu]"
+          , "docker:"
+          , "  image_cleanup: true"
+          , "  pull: false"
+          , "  restylers:"
+          , "    net_none: false"
+          , "    cpu_shares: 1024"
+          , "    memory: { number: 512, suffix: k }"
+          , "git:"
+          , "  clean: false"
+          , "  commit: false"
+          ]
+          []
+          ["Bar.hs"]
+
+      -- config.logSettings `shouldBe` _
+      config.enabled `shouldBe` False
+      config.exclude `shouldBe` ["a", "b", "c", "d"]
+      config.dryRun `shouldBe` True
+      config.failOnDifferences `shouldBe` True
+      config.commitTemplate `shouldBe` "Hi"
+      config.remoteFiles
+        `shouldBe` [RemoteFile {url = "https://example.com", path = "example.txt"}]
+      config.ignores
+        `shouldBe` Ignores
+          { byAuthor = ["x"]
+          , byBranch = ["y"]
+          , byLabels = []
+          }
+      config.restylersVersion `shouldBe` "dev"
+      config.restylersManifest `shouldBe` Nothing
+      config.restylerOverrides `shouldBe` [enabled "fourmolu"]
+      -- config.hostDirectory `shouldBe` _
+      config.imageCleanup `shouldBe` True
+      config.noCommit `shouldBe` True
+      config.noClean `shouldBe` True
+      config.noPull `shouldBe` True
+      config.restrictions
+        `shouldBe` Restrictions
+          { netNone = False
+          , cpuShares = Just 1024
+          , memory = Just $ Bytes {number = 512, suffix = Just K}
+          }
+      config.pullRequestJson `shouldBe` Nothing
+      config.paths `shouldBe` pure "Bar.hs"
+
+    context "restyler overrides" $ do
+      it "supports names" $ do
+        config <-
+          loadTestConfig
+            [ "restylers:"
+            , "  - fourmolu"
+            , "  - \"!stylish-haskell\""
+            ]
+            []
+            ["x.hs"]
+
+        config.restylerOverrides
+          `shouldBe` [enabled "fourmolu", disabled "stylish-haskell"]
+
+      it "supports name-keys" $ do
+        config <-
+          loadTestConfig
+            [ "restylers:"
+            , "  - fourmolu:"
+            , "      enabled: true"
+            , "  - stylish-haskell:"
+            , "      enabled: false"
+            ]
+            []
+            ["x.hs"]
+
+        config.restylerOverrides
+          `shouldBe` [enabled "fourmolu", disabled "stylish-haskell"]
+
+      it "supports objects with names" $ do
+        config <-
+          loadTestConfig
+            [ "restylers:"
+            , "  - name: fourmolu"
+            , "    enabled: true"
+            , "  - name: stylish-haskell"
+            , "    enabled: false"
+            ]
+            []
+            ["x.hs"]
+
+        config.restylerOverrides
+          `shouldBe` [enabled "fourmolu", disabled "stylish-haskell"]
+
+    context "legacy ignore options" $ do
+      it "fully specified" $ do
+        config <-
+          loadTestConfig
+            [ "ignore_authors: [a]"
+            , "ignore_branches: [b]"
+            , "ignore_labels: [c]"
+            ]
+            []
+            ["Bar.hs"]
+
+        config.ignores
+          `shouldBe` Ignores
+            { byAuthor = ["a"]
+            , byBranch = ["b"]
+            , byLabels = ["c"]
+            }
+
+      it "partially specified" $ do
+        config <-
+          loadTestConfig
+            [ "ignore_authors: [a]"
+            , "ignore_labels: [c]"
+            ]
+            []
+            ["Bar.hs"]
+
+        config.ignores
+          `shouldBe` Ignores
+            { byAuthor = ["a"]
+            , byBranch = ["renovate/*"]
+            , byLabels = ["c"]
+            }
 
 loadTestConfig
   :: HasCallStack
