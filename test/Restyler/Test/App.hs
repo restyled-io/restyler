643eb918e2884b1e6405c9ed7f0275860109be0a
patrick brisbin <pbrisbin@gmail.com>
2025-08-01 09:49:37 -0400

    refactor(tests): move TestApp into shared module

--- test/Restyler/Test/App.hs
+++ test/Restyler/Test/App.hs
@@ -8,6 +8,8 @@
 -- Portability : POSIX
 module Restyler.Test.App
   ( TestAppT (..)
+  , SimpleTestApp (..)
+  , withSimpleTestApp
   , shouldBe
   , shouldReturn
   , module Test.Hspec
@@ -16,13 +18,16 @@ module Restyler.Test.App
 
 import Restyler.Prelude
 
+import Blammo.Logging.LogSettings (defaultLogSettings)
+import Blammo.Logging.Logger (newTestLogger)
 import Restyler.Monad.Directory as X
 import Restyler.Monad.Docker as X
 import Restyler.Monad.DownloadFile as X
 import Restyler.Monad.Git as X
 import Restyler.Monad.ReadFile as X
 import Restyler.Monad.WriteFile as X
-import Restyler.Test.FS (ReaderFS (..))
+import Restyler.Test.FS (FS, HasFS (..), ReaderFS (..))
+import Restyler.Test.FS qualified as FS
 import Test.Hspec hiding (shouldBe, shouldReturn)
 import Test.Hspec.Core.Spec (Example (..))
 import Test.Hspec.Expectations.Lifted qualified as Lifted
@@ -54,6 +59,25 @@ instance Example (TestAppT env a) where
       (action $ \env -> void $ runReaderT f.unwrap env)
       params
       ($ ())
+
+-- | A simple test app, with a filesystem and logger
+data SimpleTestApp = SimpleTestApp
+  { logger :: Logger
+  , fs :: FS
+  }
+
+instance HasLogger SimpleTestApp where
+  loggerL = lens (.logger) $ \x y -> x {logger = y}
+
+instance HasFS SimpleTestApp where
+  fsL = lens (.fs) $ \x y -> x {fs = y}
+
+withSimpleTestApp :: SpecWith SimpleTestApp -> Spec
+withSimpleTestApp =
+  before
+    $ SimpleTestApp
+    <$> newTestLogger defaultLogSettings
+    <*> FS.build "/" []
 
 shouldBe :: (Eq a, HasCallStack, Show a) => a -> a -> TestAppT env ()
 shouldBe = Lifted.shouldBe
